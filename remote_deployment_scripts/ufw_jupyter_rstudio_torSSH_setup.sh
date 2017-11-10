d67ccae1091982a2bfaad20089b3729fa21545ef
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-11-10 12:27:29 +0200

    Added UFW status to summary

--- remote_deployment_scripts/ufw_jupyter_rstudio_torSSH_setup.sh
+++ remote_deployment_scripts/ufw_jupyter_rstudio_torSSH_setup.sh
@@ -148,6 +148,9 @@ echo
 echo "Thease are the IP addresses associated with this server"
 sudo ifconfig | awk '/inet addr/{print substr($2,6)}'
 echo
+echo "These are the UFW rules in force."
+sudo ufw status
+echo
 echo "This is the Tor hostname. Keep it a secret. You can use it to connect to this server over the Tor network."
 sudo cat /var/lib/tor/sshd/hostname
 echo
