f10003a733169634564570bffe699dc9f9da7ccd
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-11-10 12:50:09 +0200

    Break out end summary to a new file

--- remote_deployment_scripts/ufw_jupyter_rstudio_torSSH_setup.sh
+++ remote_deployment_scripts/ufw_jupyter_rstudio_torSSH_setup.sh
@@ -140,22 +140,4 @@ fi
 
 #===========================================================================#
 echo "######################### END #############################"
-echo "####################### SUMMARY ###########################"
-echo
-echo "Your username is "$username.
-echo
-echo "These are the ports currently being listened on"
-netstat -lntu
-echo
-echo "Thease are the IP addresses associated with this server"
-sudo ifconfig | awk '/inet addr/{print substr($2,6)}'
-echo
-echo "These are the UFW rules in force."
-sudo ufw status
-echo
-echo "This is the Tor hostname. Keep it a secret. You can use it to connect to this server over the Tor network."
-sudo cat /var/lib/tor/sshd/hostname
-echo
-echo "Goodbye"
-echo
-exit
+
