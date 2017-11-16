d959776f65c98388232384b2d74c373171bd2764
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-11-16 09:23:52 +0200

    Update create_user.sh

--- remote_deployment_scripts/create_user.sh
+++ remote_deployment_scripts/create_user.sh
@@ -3,6 +3,7 @@ echo "This script adds system users"
 echo "Users created with this script will have a home folder."
 echo "Their home folder will be accessible to via JupyterHub, RStudio Server and NextCloud"
 echo "They will NOT have sudo rights"
+echo "You're going tho have to enter passwords a LOT of times while this script is running. But it's relatively quick."
 echo
 echo "Enter the new user's username:"
 read added_user
@@ -36,18 +37,13 @@ else
   exit 0
 fi
 
-# To implement: set up automatic cron job to sync home folder to Nextcloud.
-# 1 Test this command
-# Add it to crontab programmatically
-# mabye you need to change to user
-# write out current crontab
-# crontab -l > mycron
-## echo new cron into cron file
-# echo "00 09 * * 1-5 echo hello" >> mycron
-## install new cron file
-# crontab mycron
-# rm mycron
-
-# I think this works
-#(crontab -l 2>/dev/null; echo "*/5 * * * * /path/to/job -with args") | crontab -
+echo "Now we're going to set up a 5 minute rpeeating task to sync "$added_user"'s NexCloud account and Home folder."
+echo
+echo "Logging into "$added_user"'s account."
+su $added_user
+echo "Adding the 5 minute schedule to the user's crontab"
+echo
+echo "Enter "$added_user"'s NextCloud account password."
+read added_password
+(crontab -l 2>/dev/null; echo "nextcloudcmd -u $added_user -p $added_password -s /home/username/ https://localhost/remote.php/webdav/") | crontab -
 #nextcloudcmd -u $added_user -p $added_password -s /home/username/ https://localhost/remote.php/webdav/
