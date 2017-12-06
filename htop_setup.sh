d7c37ef9ea4bbbeb86b755103d77d6a71c0c4586
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-12-06 16:35:09 +0200

    Create htop_setup.sh

---
+++ htop_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo
+echo "=====================> Setting up HTOP"
+echo
+sudo apt-get update
+sudo apt-get install htop -y
+echo
+#=================================================================#
+echo
