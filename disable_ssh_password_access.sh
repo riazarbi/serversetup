fe880c79d6f7c5870d249b366a57f213a4615587
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-12-06 16:33:15 +0200

    Create disable_ssh_password_access.sh

---
+++ disable_ssh_password_access.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo
+
+echo "=====================> Disabling password-based authentication for SSH"
+sudo sed -i '/PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
+sudo service ssh restart
+echo
+#=================================================================#
+
+echo
