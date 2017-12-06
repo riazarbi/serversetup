3c5cae8e742392b35d54a11bfe59bf72f876abe5
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-12-06 21:43:42 +0200

    Update nextcloud_setup.sh

--- remote_deployment_scripts/nextcloud_setup.sh
+++ remote_deployment_scripts/nextcloud_setup.sh
@@ -17,17 +17,20 @@ sudo snap install nextcloud
 snap changes nextcloud
 snap info nextcloud
 snap interfaces nextcloud
+echo
+
 echo "=====================> Setting up Admin user for Nextcloud"
 sudo nextcloud.manual-install $USER $adminpassword
 echo
+
 echo "=====================> Setting up this server's IP address as a trusted domain"
 sudo nextcloud.occ config:system:get trusted_domains
 sudo nextcloud.occ config:system:set trusted_domains 1 --value=$ServerIP
 sudo nextcloud.occ config:system:get trusted_domains
 sudo nextcloud.enable-https self-signed
 echo
-#=================================================================#
 
+#=================================================================#
 echo "=====================> Setting up NextCloud Client"
 echo
 sudo add-apt-repository ppa:nextcloud-devs/client
