75efd1fa3270b022a881311cfaba5bf1c93b6600
Riaz Arbi <riazarbi@users.noreply.github.com>
2017-12-06 16:36:47 +0200

    Create UFW_setup.sh

---
+++ UFW_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+echo "=====================> Setting up UFW"
+echo
+sudo apt-get install ufw -y
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+echo "Opening up ports 22 for SSH access"
+sudo ufw allow ssh
+echo
+echo "y" | sudo ufw enable
+#=================================================================#
+echo
